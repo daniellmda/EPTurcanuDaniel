bc459171d0b8d06bad67f1a6288cca75f7251552
Daniel <danielturcanu15@gmail.com>
2025-04-09 14:23:22 +0300

    TASK6 refactor mergeObjects function to use object spread syntax for simplicity

--- TASK6/script.js
+++ TASK6/script.js
@@ -1,19 +1,7 @@
 function mergeObjects(obj1, obj2) {
-    const mergedArr = {};
-  
-    for (const key in obj1) {
-      mergedArr[key] = obj1[key];
-    }
-  
-    for (const key in obj2) {
-      mergedArr[key] = obj2[key];
-    }
-  
-    return mergedArr;
-  }
-  
+  return { ...obj1, ...obj2 };
+}
 
-  const obj1 = {a: 1, b: 4};
-  const obj2 = {b: 6, c: 2};
-  console.log(mergeObjects(obj1, obj2));  
-  
+const obj1 = { a: 1, b: 4 };
+const obj2 = { b: 6, c: 2 };
+console.log(mergeObjects(obj1, obj2));
