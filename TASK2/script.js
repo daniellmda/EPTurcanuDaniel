4363532de9f31919807c5f142f9f0fd0b9101a45
Daniel <danielturcanu15@gmail.com>
2025-04-09 13:29:40 +0300

    TASK2 implement isPrime function

---
+++ TASK2/script.js
@@ -0,0 +1,28 @@
+function isPrime(n) {
+  const sqrt = Math.sqrt(n);
+  if (n < 2) {
+    return false;
+  }
+
+  if (n === 2) {
+    return true;
+  }
+
+  if (n % 2 === 0) {
+    return false;
+  }
+
+  for (let i = 3; i <= sqrt; i += 2) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isPrime(8));
+console.log(isPrime(12));
+console.log(isPrime(2));
+console.log(isPrime(0));
+console.log(isPrime(83));
