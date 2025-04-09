3b6507bfce33b78d57bcd46af55ad8595ddc2a92
Daniel <danielturcanu15@gmail.com>
2025-04-09 14:28:57 +0300

    TASK2 refactor isPrime function for improved efficiency and readability

--- TASK2/script.js
+++ TASK2/script.js
@@ -1,23 +1,8 @@
 function isPrime(n) {
-  if (n < 2) {
-    return false;
-  }
-
-  if (n === 2) {
-    return true;
-  }
-
-  if (n % 2 === 0) {
-    return false;
-  }
-
-  for (let i = 3; i <= Math.sqrt(n); i += 2)    {
-    if (n % i === 0) {
-      return false;
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) return false;
     }
-  }
-
-  return true;
+    return n > 1;
 }
 
 console.log(isPrime(8));
