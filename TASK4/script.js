7cfa4d2141a5796eb64f45a5eeabc1d498112d1a
Daniel <danielturcanu15@gmail.com>
2025-04-09 14:39:19 +0300

    TASK8 Implemented startLoading function, also fixed TASK4 refactor countOccurrences function for improved readability and consistency;

--- TASK4/script.js
+++ TASK4/script.js
@@ -1,13 +1,8 @@
 function countOccurrences(arr, value) {
-    return arr.reduce((acc, curr) => {
-        if (curr === value) {
-            acc++;
-        }
-        return acc;
-    }, 0); 
+    return arr.reduce((acc, curr) => curr === value ? acc + 1 : acc, 0);
 }
 
-console.log(countOccurrences([1, 2, 2, 3, 2, 4], 2));
-console.log(countOccurrences(["a", "b", "a", "c"], "a"));
-console.log(countOccurrences([5, 5, 5, 5], 5));
-console.log(countOccurrences([1, 2, 3, 4], 5));
+console.log(countOccurrences([1, 2, 2, 3, 2, 4], 2)); 
+console.log(countOccurrences(["a", "b", "a", "c"], "a")); 
+console.log(countOccurrences([5, 5, 5, 5], 5)); 
+console.log(countOccurrences([1, 2, 3, 4], 5)); 
